a9b31e9c9ceec570dd2d91d052f523b44f94601e
akanasu46 <145829290+akanasu46@users.noreply.github.com>
2025-12-25 16:56:35 +0900

    Update data.js

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 const ARTICLES = [
-    { id: 1, date: "2025.12.25", title: "サイトをオープンしました！", content: "サイトオープンの備忘録<p>GitHubアカウントを作成。<p>新しいリポジトリを作成。" },
+    { id: 1, date: "2025.12.25", title: "サイトをオープンしました！", content: "サイトオープンの備忘録<p><strong>1. GitHubアカウントを作成。</strong>：Githubに登録する。<p><strong>2. 新しいリポジトリを作成</strong>：「New repository」ボタンを押し、名前（例：my-site）を決めて「Public」で作成します。<p><strong>3. ファイルをアップロード</strong>：作成したリポジトリに、PCで作ったファイル（html, css, js, articlesフォルダ）をすべてドラッグ＆ドロップでアップロードします。" },
     { id: 2, date: "2025.12.20", title: "冬の北海道一人旅", content: "マイナス10度の世界。ラーメンが格別でした。" },
     { id: 3, date: "2025.12.17", title: "こんにちは", content: "こんにちは。Hello。你好。" }
 ];
@@ -13,3 +13,4 @@ const DIARIES = [
     }
 
 ];
+
